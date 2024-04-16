378e97cf89d686e70d4eedc600dc4ed89ee7ad2e
qarksangdol <0629psw@gmail.com>
2024-04-16 17:50:43 +0900

    클론페이지 일부

---
+++ script/nav.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY === 0) {
+        navbar.style.borderBottom = 'none'; 
+        navbar.style.backgroundColor = '';
+    } else {
+        navbar.style.borderBottom = '1px solid rgb(230, 228, 228)';
+        navbar.style.backgroundColor = 'white';  
+    }
+});
