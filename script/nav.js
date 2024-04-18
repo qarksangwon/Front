ee04c38f36d66861844ff6b6c8bb4ce2916e7b25
qarksangwon <0629psw@gmail.com>
2024-04-18 22:54:13 +0900

    클론페이지 js 수정

--- script/nav.js
+++ script/nav.js
@@ -1,3 +1,11 @@
+// 새로고침 시 맨위 로딩, 맨 마지막에 넣을거임
+// window.onload = function() {
+//     window.scrollTo(0, 0);
+// };
+// window.addEventListener('beforeunload', function() {
+//     window.scrollTo(0, 0);
+// });
+
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
 
@@ -46,13 +54,13 @@ document.addEventListener('DOMContentLoaded', () => {
             title2.classList.add('opacity-visible');
             setTimeout(() => {
                 img2.classList.add('opacity-visible');
-            }, 800);
+            }, 500);
             setTimeout(() => {
                 img1.classList.add('opacity-visible');
-            }, 1600);
+            }, 1000);
             setTimeout(() => {
                 paragraph.classList.add('opacity-visible');
-            }, 2400);
+            }, 1500);
         }
     });
 });
@@ -61,32 +69,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const section3 = document.querySelector('.section3');
     const title1 = document.querySelector('.section3-1-h1');
     const title2 = document.querySelector('.section3-1-h2');
-    const pElements = document.querySelectorAll('.section3 .section3-2 .section3-2-left p, .section3 .section3-2 .section3-3-right p, .section3 .section3-4 .section3-4-left p');
-    const imgElements = document.querySelectorAll('.section3 .section3-2 .section3-2-left img, .section3 .section3-2 .section3-2-right img, .section3 .section3-3 .section3-3-left img, .section3 .section3-4 .section3-4-left img, .section3 .section3-4 .section3-4-right img');
+    const pElements = document.querySelectorAll('.section3 .section3-2 .section3-2-left p, .section3 .section3-3 .section3-3-right p, .section3 .section3-4 .section3-4-left p');
+    const imgElements = document.querySelectorAll('.section3 .section3-2 .section3-2-right img, .section3 .section3-3 .section3-3-left img, .section3 .section3-4 .section3-4-right img');
     
     let section3Visible = false;
 
     window.addEventListener('scroll', () => {
-        const section3Top = section3.getBoundingClientRect().top * 2;
+        const section3Top = section3.getBoundingClientRect().top * 6;
         const scrollPosition = window.scrollY;
 
         if (scrollPosition >= section3Top && !section3Visible) {
             section3Visible = true;
 
-            title1.classList.add('opacity-visible');
-            title2.classList.add('opacity-visible');
+            setTimeout(() => {
+                title1.classList.add('opacity-visible');
+                title2.classList.add('opacity-visible');
+            }, 200);
 
-            pElements.forEach((p, index) => {
-                setTimeout(() => {
+            setTimeout(() => {
+                pElements.forEach((p) => {
                     p.classList.add('opacity-visible');
-                }, 800 * (index + 1)); // 각 p 태그에 대해 800ms 간격으로 표시
-            });
+                });
+            }, 1200);
 
             setTimeout(() => {
                 imgElements.forEach((img) => {
                     img.classList.add('opacity-visible');
                 });
-            }, 800 * pElements.length + 800); // 모든 p 태그가 표시된 후 800ms 뒤에 img 태그 표시
+            }, 1500);
         }
     });
 });
